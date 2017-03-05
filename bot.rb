34336da4e2fcb2c920b1975b0886bb8482071105
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2017-03-05 14:07:32 -0500

    Search further

--- bot.rb
+++ bot.rb
@@ -1,8 +1,23 @@
 require 'facebook/messenger'
 require 'open-uri'
 require 'json'
+require 'redis'
 
 include Facebook::Messenger
+
+$redis = Redis.new(host: 'redis')
+
+def get_session(id)
+  session = $redis.get(id)
+  if session.nil?
+    return {}
+  else
+    return JSON.parse session end
+end
+
+def set_session(id, data)
+  $redis.set(id, data.to_json)
+end
 
 
 # SEND API
@@ -129,31 +144,52 @@ end
 
 # END
 
-Bot.on :postback do |postback|
+def search_and_show_results(message, distance, session)
 
-  postback.ask_for_location('Dónde estas ubicado? Dinos y te mostraremos dónde están los puntos de recarga TuLlave más cercanos.')
+    lat = session["lat"]
+    lng = session["lng"]
 
-end
-
-Bot.on :message do |message|
-
-  if message.is_location_attachment?
-
-    message.show_typing(true)
-
-    lat, lng = message.location
-  
-    open("https://easitp.ylecuyer.xyz/tullave/#{lat}/#{lng}/500") do |data|
+    open("https://easitp.ylecuyer.xyz/tullave/#{lat}/#{lng}/#{distance}") do |data|
       array = JSON.parse(data.string)
 
       unless array.count > 0
         message.reply_with_text("\u{1F623}")
         message.reply_with_text("No hay puntos de recarga cercanos abiertos a esta hora.")
+        message.reply({
+            text: "Buscar más lejos?",
+            quick_replies:[
+              {
+                content_type: "text",
+                title: "750m",
+                payload: "750M",
+              },
+              {
+                content_type: "text",
+                title: "1km",
+                payload: "1KM",
+              },
+              {
+                content_type: "text",
+                title: "2km",
+                payload: "2KM",
+              },
+              {
+                content_type: "text",
+                title: "5km",
+                payload: "5KM",
+              },
+              {
+                content_type: "text",
+                title: "No",
+                payload: "0KM",
+              },
+            ]
+          })
       else
 
         elements = []
 
-        array.each do |punto|
+        array.take(4).each do |punto|
           elements << {
             title: punto['name'],
             subtitle: punto['address'],
@@ -182,9 +218,48 @@ Bot.on :message do |message|
 
     end
 
+end
+
+Bot.on :postback do |postback|
+
+  case postback.payload 
+  when "GET_STARTED_PAYLOAD"
+  postback.ask_for_location('Dónde estas ubicado? Dinos y te mostraremos dónde están los puntos de recarga TuLlave más cercanos.')
+  end
+
+end
+
+Bot.on :message do |message|
+
+  sender_id = message.sender["id"]
+  session = get_session(sender_id)
+
+  if message.is_location_attachment?
+
+    message.show_typing(true)
+
+    lat, lng = message.location
+
+    session["lat"] = lat
+    session["lng"] = lng
+  
+    search_and_show_results(message, 500, session)
+
+  elsif ["750M", "1KM", "2KM", "5KM"].include?(message.quick_reply)
+    case message.quick_reply 
+    when "750M"
+      search_and_show_results(message, 750, session)
+    when "1KM"
+      search_and_show_results(message, 1000, session)
+    when "2KM"
+      search_and_show_results(message, 2000, session)
+    when "5KM"
+      search_and_show_results(message, 5000, session)
+    end
   else
     message.ask_for_location('Dónde estas ubicado? Dinos y te mostraremos dónde están los puntos de recarga TuLlave más cercanos.')
   end
 
+  set_session sender_id, session
 
 end
